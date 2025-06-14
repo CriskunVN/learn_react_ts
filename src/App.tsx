fd828494abb7eade88a4da8f0b43c81de5e99a2b
CriskunVN <phukun0404@gmail.com>
2025-06-14 23:38:43 +0700

    learn useState to rerender component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name , setName] = useState('React');
+  const changeName = () => { 
+    setName('React TS 2025')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={changeName}>Ok</button>
+      <h1>{name}</h1>
     </div>
   );
 }
