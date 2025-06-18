2b6bd11d5efdf794a0145247a75c304a3ff84e7b
CriskunVN <phukun0404@gmail.com>
2025-06-19 02:49:22 +0700

    create function props to send data from child component to parent component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,6 +2,7 @@
 import CardPizza from "../sections/CardPizza";
 import { useState } from "react";
 import { Pizza } from "../models/pizza.model";
+import ItemPizza from "../sections/ItemPizza";
 
 const Homepage = () => { 
 
@@ -12,13 +13,24 @@ const Homepage = () => {
         { id: 4, title: "Veggie Supreme", description: "A mix of bell peppers, onions, mushrooms, and olives on a tomato base." }
     ]);
 
+    const [person , setPerson] = useState<Pizza>({
+        title: 'Kun Dev',
+        description: 'Pizza Lover and Developer'
+    });
+
+    const handleChangePerson = (data : Pizza) => { 
+        setPerson(data);
+    }
+
+
     return (
         <div style={ {height: 'calc(100vh - 309px)', padding: '4rem 4rem', overflow:'auto'} }>
             <div className="wrapper-card-items">
              {
                 pizzas.map( item => <CardPizza id={item.id} title={item.title} description={item.description}/>)
              }       
-        
+
+             <ItemPizza title={person.title} description={person.description} handleChangePerson={handleChangePerson} />
             
                 
             </div>
