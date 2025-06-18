f676957556a8efc58c03402b9a0b059fd28d1ae2
CriskunVN <phukun0404@gmail.com>
2025-06-19 03:06:39 +0700

    use function props to delete element in pizzas

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,41 +1,63 @@
+import CardPizza from '../sections/CardPizza';
+import { useState } from 'react';
+import { Pizza } from '../models/pizza.model';
+import ItemPizza from '../sections/ItemPizza';
 
-import CardPizza from "../sections/CardPizza";
-import { useState } from "react";
-import { Pizza } from "../models/pizza.model";
-import ItemPizza from "../sections/ItemPizza";
+const Homepage = () => {
+  const [pizzas, setPizzas] = useState<Pizza[]>([
+    {
+      id: 1,
+      title: 'Margherita',
+      description:
+        'Classic pizza with tomato sauce, mozzarella cheese, and fresh basil.',
+    },
+    {
+      id: 2,
+      title: 'Pepperoni',
+      description:
+        'Spicy pepperoni slices with mozzarella cheese and tomato sauce.',
+    },
+    {
+      id: 3,
+      title: 'BBQ Chicken',
+      description: 'Grilled chicken with BBQ sauce, red onions, and cilantro.',
+    },
+    {
+      id: 4,
+      title: 'Veggie Supreme',
+      description:
+        'A mix of bell peppers, onions, mushrooms, and olives on a tomato base.',
+    },
+  ]);
 
-const Homepage = () => { 
+  const handleRemovePizza = (id: number) => {
+    const indexPizza = pizzas.findIndex((item) => item.id === id);
+    let newPizzas = [...pizzas];
+    newPizzas.splice(indexPizza, 1);
+    setPizzas(newPizzas);
+  };
 
-    const [pizzas, setPizzas] = useState<Pizza[]>([
-        { id: 1, title: "Margherita", description: "Classic pizza with tomato sauce, mozzarella cheese, and fresh basil." },
-        { id: 2, title: "Pepperoni", description: "Spicy pepperoni slices with mozzarella cheese and tomato sauce." },
-        { id: 3, title: "BBQ Chicken", description: "Grilled chicken with BBQ sauce, red onions, and cilantro." },
-        { id: 4, title: "Veggie Supreme", description: "A mix of bell peppers, onions, mushrooms, and olives on a tomato base." }
-    ]);
-
-    const [person , setPerson] = useState<Pizza>({
-        title: 'Kun Dev',
-        description: 'Pizza Lover and Developer'
-    });
-
-    const handleChangePerson = (data : Pizza) => { 
-        setPerson(data);
-    }
-
-
-    return (
-        <div style={ {height: 'calc(100vh - 309px)', padding: '4rem 4rem', overflow:'auto'} }>
-            <div className="wrapper-card-items">
-             {
-                pizzas.map( item => <CardPizza id={item.id} title={item.title} description={item.description}/>)
-             }       
-
-             <ItemPizza title={person.title} description={person.description} handleChangePerson={handleChangePerson} />
-            
-                
-            </div>
-        </div>
-    );
-}
+  return (
+    <div
+      style={{
+        height: 'calc(100vh - 309px)',
+        padding: '4rem 4rem',
+        overflow: 'auto',
+      }}
+    >
+      <div className="wrapper-card-items">
+        {pizzas.map((item) => (
+          <CardPizza
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            handleRemovePizza={handleRemovePizza}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
--- src/sections/CardPizza.tsx
+++ src/sections/CardPizza.tsx
@@ -1,24 +1,31 @@
-
 import LogoPizza from '../assets/pizza.jpg';
-import { Pizza } from "../models/pizza.model";
+import { Pizza } from '../models/pizza.model';
 
-type Props = Pizza;
+type Props = Pizza & {
+  handleRemovePizza: (id: number) => void;
+};
 
-const CardPizza = ({title, description} : Props) => { 
-
-    return (
-        <div className="card-items">
-                    <img className="card-image-pizza" src={LogoPizza} alt='logopizza' />
-                    <div style={{height: '50%' , width: '100%'}}></div>
-                    <div className="card-content">
-                        <div>
-                            <div style={{fontSize: '20px' , fontWeight:600 , color: '#fff'}}>{title}</div>
-                            <div style={{fontSize: '20px' , fontWeight:300 , color: '#fff'}}>{description}</div>
-
-                        </div>
-                    </div>
-                </div>
-    );
-}
+const CardPizza = ({ id, title, description, handleRemovePizza }: Props) => {
+  return (
+    <div
+      className="card-items"
+      onClick={() => handleRemovePizza(id!)}
+      style={{ cursor: 'pointer' }}
+    >
+      <img className="card-image-pizza" src={LogoPizza} alt="logopizza" />
+      <div style={{ height: '50%', width: '100%' }}></div>
+      <div className="card-content">
+        <div>
+          <div style={{ fontSize: '20px', fontWeight: 600, color: '#fff' }}>
+            {title}
+          </div>
+          <div style={{ fontSize: '20px', fontWeight: 300, color: '#fff' }}>
+            {description}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CardPizza;
